b341b0f45f437f5886768abd58c089018230c954
TatianaNai <tatyana_tat333@mail.ru>
2025-08-28 21:06:37 +0300

    Добавлен базовый тестовый класс

--- src/test/java/ru/kinopoisk/BaseTest.java
+++ src/test/java/ru/kinopoisk/BaseTest.java
@@ -1,4 +1,20 @@
 package ru.kinopoisk;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import ru.kinopoisk.driverManagers.Driver;
+import ru.kinopoisk.utils.Props;
+
 public class BaseTest {
+    @BeforeClass
+    public void setUp() {
+        Driver.INSTANCE.getDriver();
+        Driver.INSTANCE.getDriver().manage().window().maximize();
+        Driver.INSTANCE.getDriver().get(Props.getProperty("url"));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        Driver.INSTANCE.quit();
+    }
 }
