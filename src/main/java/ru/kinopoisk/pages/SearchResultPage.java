3ce09e9e5c2db063452ce1a7b638829b5326ab75
TatianaNai <tatyana_tat333@mail.ru>
2025-08-29 12:57:14 +0300

    Добавлена страница SearchResultPage

---
+++ src/main/java/ru/kinopoisk/pages/SearchResultPage.java
@@ -0,0 +1,35 @@
+package ru.kinopoisk.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SearchResultPage extends BasePage{
+    @FindBy(xpath = "//*[contains(@class,'element most_wanted')]//*[contains(@class,'pic')]")
+    private WebElement mostWantedMovieImage;
+    @FindBy(xpath = "//*[contains(@class,'element most_wanted')]//*[contains(@class,'name')]//*[contains(@class,'js-serp-metrika')]")
+    private WebElement mostWantedMovieTitle;
+    @FindBy(xpath = "//*[contains(@class,'element most_wanted')]//*[contains(@class,'year')]")
+    private WebElement mostWantedMovieReleaseYear;
+
+    public SearchResultPage() {
+        super(By.xpath("//*[contains(@class,'search_results_topText')]"));
+    }
+
+    public void clickMostWantedMovieImage() {
+        mostWantedMovieImage.click();
+    }
+
+    public String getMostWantedMovieName() {
+        return mostWantedMovieTitle.getText();
+    }
+
+    public int getMostWantedMovieReleaseYear() {
+        String movieReleaseYearString= mostWantedMovieReleaseYear.getText();
+        if (!movieReleaseYearString.matches("\\d+")) {
+            throw new IllegalArgumentException("Only numbers are expected in most wanted movie's release year: " + movieReleaseYearString);
+        }
+
+        return Integer.parseInt(movieReleaseYearString);
+    }
+}
