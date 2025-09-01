d150258ef59d8a7cd928dfb83a9e2c254ce4c015
TatianaNai <tatyana_tat333@mail.ru>
2025-09-01 10:34:08 +0300

    Добавлен метод по получению названия и года выхода фильма

--- src/main/java/ru/kinopoisk/pages/MoviePage.java
+++ src/main/java/ru/kinopoisk/pages/MoviePage.java
@@ -3,6 +3,9 @@ package ru.kinopoisk.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MoviePage extends BasePage implements PagesUniqueElement{
     @FindBy(xpath = "//*[@itemprop='name']")
     private WebElement movieTitle;
@@ -12,15 +15,20 @@ public class MoviePage extends BasePage implements PagesUniqueElement{
     }
 
     public String getMovieName() {
-        String movieNameAndReleaseYear = movieTitle.getText();
-        int openBracketIndex = movieNameAndReleaseYear.indexOf("(");
-        return movieNameAndReleaseYear.substring(0, openBracketIndex).trim();
+        return getMovieNameAndReleaseYear().get("name");
     }
 
     public int getMovieReleaseYear() {
+        return Integer.parseInt(getMovieNameAndReleaseYear().get("year"));
+    }
+
+    private Map<String, String> getMovieNameAndReleaseYear() {
         String movieNameAndReleaseYear = movieTitle.getText();
         int openBracketIndex = movieNameAndReleaseYear.indexOf('(');
         int closeBracketIndex = movieNameAndReleaseYear.indexOf(')');
-        return Integer.parseInt(movieNameAndReleaseYear.substring(openBracketIndex + 1, closeBracketIndex));
+        Map<String, String> moviesInfo = new HashMap<>();
+        moviesInfo.put("name", movieNameAndReleaseYear.substring(0, openBracketIndex).trim());
+        moviesInfo.put("year", movieNameAndReleaseYear.substring(openBracketIndex + 1, closeBracketIndex));
+        return moviesInfo;
     }
 }
