286cd47db37920303856d05111b124a065c887b2
TatianaNai <tatyana_tat333@mail.ru>
2025-09-02 13:18:08 +0300

    Добавлено логирование в метод getMovieNameAndReleaseYear в странице MoviePage

--- src/main/java/ru/kinopoisk/pages/MoviePage.java
+++ src/main/java/ru/kinopoisk/pages/MoviePage.java
@@ -1,11 +1,13 @@
 package ru.kinopoisk.pages;
 
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public class MoviePage extends BasePage {
     @FindBy(xpath = "//*[@itemprop='name']")
     private WebElement movieTitle;
@@ -23,11 +25,15 @@ public class MoviePage extends BasePage {
     }
 
     private Map<String, String> getMovieNameAndReleaseYear() {
+        log.info("Getting movie name and release year from movieTitle element");
         String movieNameAndReleaseYear = movieTitle.getText();
         int openBracketIndex = movieNameAndReleaseYear.indexOf('(');
         int closeBracketIndex = movieNameAndReleaseYear.indexOf(')');
+        log.info("Open bracket index: {}, close bracket index: {}", openBracketIndex, closeBracketIndex);
         Map<String, String> moviesInfo = new HashMap<>();
+        log.info("Adding movie name to HashMap");
         moviesInfo.put("name", movieNameAndReleaseYear.substring(0, openBracketIndex).trim());
+        log.info("Adding movie release year to HashMap");
         moviesInfo.put("year", movieNameAndReleaseYear.substring(openBracketIndex + 1, closeBracketIndex));
         return moviesInfo;
     }
