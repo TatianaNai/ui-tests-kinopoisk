ac2443963394b7c57dcfc9d61296552267f6b637
TatianaNai <tatyana_tat333@mail.ru>
2025-09-03 15:30:17 +0300

    Добавлен поиск уникального элемента в BasePage

--- src/main/java/ru/kinopoisk/pages/BasePage.java
+++ src/main/java/ru/kinopoisk/pages/BasePage.java
@@ -1,4 +1,15 @@
 package ru.kinopoisk.pages;
 
-public abstract class BasePage {
+import com.codeborne.selenide.SelenideElement;
+
+public abstract class BasePage implements PagesUniqueElement {
+    private final SelenideElement uniqueElement;
+
+    public BasePage(SelenideElement uniqueElement) {
+        this.uniqueElement = uniqueElement;
+    }
+
+    public boolean isPageOpen() {
+        return uniqueElement.isDisplayed();
+    }
 }
