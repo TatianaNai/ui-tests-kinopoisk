0e3762fc322f43f7397c5e0076db1fe2bab422a0
TatianaNai <tatyana_tat333@mail.ru>
2025-09-03 15:31:02 +0300

    Добавлены настройки для браузера

--- src/test/java/ru/kinopoisk/BaseTest.java
+++ src/test/java/ru/kinopoisk/BaseTest.java
@@ -1,4 +1,20 @@
 package ru.kinopoisk;
 
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import ru.kinopoisk.utils.Props;
+import ru.kinopoisk.utils.SelenideConfig;
+
 public abstract class BaseTest {
+    @BeforeEach
+    public void setUp() {
+        SelenideConfig.setUp();
+        Selenide.open(Props.getProperty("url"));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Selenide.closeWebDriver();
+    }
 }
